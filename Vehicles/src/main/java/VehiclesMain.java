6c6e555ff1732fbc5ddc9a59f911d836dab22cca
krosengr4 <rosenkev4@gmail.com>
2025-05-12 13:14:31 -0500

    Completed vehicles exercise in WB 5 mod 1

--- Vehicles/src/main/java/VehiclesMain.java
+++ Vehicles/src/main/java/VehiclesMain.java
@@ -6,6 +6,20 @@ public class VehiclesMain {
         slowRide.setColor("Red");
         slowRide.setFuelCapacity(5);
 
+        Hovercraft hoverRide = new Hovercraft();
+        hoverRide.setColor("Blue");
+        hoverRide.setNumberOfPassengers(1);
+
+        Car speedyCar = new Car();
+        speedyCar.setColor("Yellow");
+        speedyCar.setFuelCapacity(35);
+
+        SemiTruck megaTruck = new SemiTruck();
+        megaTruck.setCargoCapacity(24);
+
+        System.out.println("My semi truck can hold " + megaTruck.getCargoCapacity() + " tons.");
+        System.out.println("My car is " + speedyCar.getColor() + " and has a fuel capacity of " + speedyCar.getFuelCapacity());
+        System.out.println("My hovercraft is " + hoverRide.getColor() + " but can only hold " + hoverRide.getNumberOfPassengers() + " passenger.");
         System.out.println("My moped is " + slowRide.getColor() + " and has a fuel capacity of " + slowRide.getFuelCapacity());
     }
 
