dec0adcb160292aa123c65cb83441d25ee392a83
krosengr4 <rosenkev4@gmail.com>
2025-05-13 13:29:32 -0500

    Completed Asset Manager exercise 1 WB 5 Module 3

---
+++ AssetManager/src/main/java/Asset.java
@@ -0,0 +1,42 @@
+public class Asset {
+
+    String description;
+    String dateAcquired;
+    double originalCost;
+
+    public Asset(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    //region getters and setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAcquired() {
+        return dateAcquired;
+    }
+
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+
+    public double getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(double originalCost) {
+        this.originalCost = originalCost;
+    }
+    //endregion
+
+    public double getValue() {
+        return originalCost;
+    }
+}
---
+++ AssetManager/src/main/java/AssetVehicle.java
@@ -0,0 +1,78 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class AssetVehicle extends Asset{
+
+    String makeModel;
+    int year;
+    int odometer;
+
+    public AssetVehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    //region getters and setters
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+    //endregion
+
+    @Override
+    public double getValue() {
+        double totalValue = 0;
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter yearFormat = DateTimeFormatter.ofPattern("yyyy");
+        int currentYear = Integer.parseInt(currentDate.format(yearFormat));
+
+        int carAge = currentYear - this.year;
+
+
+        switch (carAge) {
+            case 0, 1, 2, 3:
+                totalValue += this.originalCost - (.03 * this.originalCost);
+                break;
+            case 4, 5, 6:
+                totalValue += this.originalCost - (.06 * this.originalCost);
+                break;
+            case 7, 8, 9, 10:
+                totalValue += this.originalCost - (.08 * this.originalCost);
+                break;
+            default:
+                if (carAge < 0) {
+                    throw new RuntimeException("Cannot have negative car age!");
+                }
+                totalValue += 1000.00;
+        }
+
+        if (!this.makeModel.contains("Honda") && !this.makeModel.contains("Toyota")) {
+            if (odometer > 100000) {
+                totalValue = this.originalCost - (this.originalCost * .25);
+            }
+        }
+
+        return totalValue;
+    }
+}
